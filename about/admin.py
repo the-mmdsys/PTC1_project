0c651f92d2b212843764bb0b370fbe64f092749a
the-mmdsys <sp.mmdsys@gmail.com>
2026-08-13 17:55:21 +0330

    Admin panel

--- about/admin.py
+++ about/admin.py
@@ -4,13 +4,11 @@ from .models import History, TeamMember
 
 @admin.register(History)
 class HistoryAdmin(TranslationAdmin):
-    # از فیلدهای واقعی date و title که در مدلت نوشتی استفاده کردیم
     list_display = ('title', 'date')
     search_fields = ('title', 'achievement')
     list_filter = ('date',)
 
 @admin.register(TeamMember)
 class TeamMemberAdmin(TranslationAdmin):
-    # از فیلدهای واقعی full_name و position استفاده کردیم
     list_display = ('full_name', 'position')
     search_fields = ('full_name', 'position', 'bio')
